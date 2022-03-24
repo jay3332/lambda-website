3005a2077a04ef0ad1022e3273182aeb94bcb976
jay3332 <40323796+jay3332@users.noreply.github.com>
2022-03-24 19:33:38 -0400

    Base app

---
+++ src/app/App.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { createGlobalStyle } from 'styled-components';
+
+import Title from '../components/Title';
+import Homepage from '../pages/Homepage';
+
+const GlobalStyle = createGlobalStyle`
+    :root {
+        --font-primary: 'Inter', serif;
+        --theme-background: #1a1d1d;
+        --theme-text: #ffffff;
+        --theme-text-secondary: #797979;
+        --theme-link: #2fbdff;
+        --theme-link-hover: #6deeff;
+        --theme-primary-dark: #404249;
+        --theme-primary-dark-hover: #5d5d5d;
+        --theme-primary: #4273ac;
+        --theme-primary-hover: #518dd3;
+    }
+    
+    * {
+        font-family: var(--font-primary);
+        color: var(--theme-text);
+    }
+    
+    body {
+        background: black;
+        width: 100vw;
+        margin: 0;
+    }
+    
+    a {
+        color: var(--theme-link);
+        transition: color 0.3s ease;
+    }
+    
+    a:hover {
+        color: var(--theme-link-hover);
+    }
+    
+    button {
+        cursor: pointer;
+        border: none;
+        background-color: var(--theme-primary);
+        color: var(--theme-text);
+        padding: 0.2em 0.5em;
+        border-radius: 0.2em;
+        transition: background-color 0.3s ease;
+    }
+
+    button:hover {
+        background-color: var(--theme-primary-hover);
+    }
+`;
+
+export default function App() {
+    return (
+        <>
+            <GlobalStyle />
+            <Title>Lambda</Title>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Homepage />}>
+                        <Route index element={<Homepage />}/>
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
+}
---
+++ src/pages/Homepage.tsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+import Title from '../components/Title';
+
+import LambdaIconSource from '../assets/lambda.png';
+
+const Banner = styled.div`
+    display: flex;
+    width: 100vw;
+    height: 20vmax;
+    align-items: center;
+    justify-content: center;
+`;
+
+const LambdaIcon = styled.img`
+    border-radius: 50%;
+    width: min(12.5vmax, 240px);
+`;
+
+const BannerContent = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: center;
+    margin: 2vmax;
+`;
+
+const LambdaHeader = styled.div`
+    font-size: min(3.5vmax, 64px);
+    font-weight: bold;
+    margin-bottom: min(0.75vmax, 12px);
+`;
+
+const LambdaDescription = styled.div`
+    font-size: min(1.75vmax, 36px);
+`;
+
+const BannerButtons = styled.div`
+    display: flex;
+    flex-direction: row;
+    margin-top: 1.5vmax;
+
+    a {
+        margin-right: 1vmax;
+        font-size: 1.2em;
+        padding: 0.7em 1.1em;
+        background-color: var(--theme-primary);
+        color: var(--theme-text);
+        border-radius: 0.2em;
+        transition: background-color 0.3s ease;
+        text-decoration: none;
+        cursor: pointer;
+    }
+
+    a:hover {
+        background-color: var(--theme-primary-hover);
+        color: var(--theme-text);
+    }
+`;
+
+const BannerDashboardButton = styled(Link)`
+    background-color: var(--theme-primary-dark) !important;
+
+    &:hover {
+        background-color: var(--theme-primary-dark-hover) !important;
+    }
+`;
+
+export default function Homepage() {
+    return (
+        <>
+            <Title>Lambda: Homepage</Title>
+            <Banner>
+                <LambdaIcon src={LambdaIconSource} />
+                <BannerContent>
+                    <LambdaHeader>Lambda</LambdaHeader>
+                    <LambdaDescription>A multipurpose bot for Discord.</LambdaDescription>
+                    <BannerButtons>
+                        <a href="https://discord.com/oauth2/authorize?client_id=734872509912186921&scope=bot+applications.commands&permissions=8">
+                            Add Lambda
+                        </a>
+                        <BannerDashboardButton to="/dashboard">Dashboard</BannerDashboardButton>
+                    </BannerButtons>
+                </BannerContent>
+            </Banner>
+        </>
+    );
+}
